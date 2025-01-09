ffe09e9f695942f1f07f8ac87ff1ba65fb0d5d72
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2025-01-09 00:12:58 -0300

    sem primeng

--- cbd_vital/src/app/components/feedbacks/feedbacks.component.ts
+++ cbd_vital/src/app/components/feedbacks/feedbacks.component.ts
@@ -1,37 +1,37 @@
 import { Component } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
+// import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-feedbacks',
-  imports: [CarouselModule],
+  imports: [],
   templateUrl: './feedbacks.component.html',
 })
 export class FeedbacksComponent {
 
-  feedbacks = [
-    {
-      name: 'Laércio da Costa',
-      text: 'Tenho diabetes tipo 2 e o óleo de CBD, com auxílio de um médico, me ajudou muito, pois as dores eram terríveis.',
-      image: 'assets/laercio.jpg'
-    },
-    {
-      name: 'Fernanda Silva',
-      text: 'Minha mãe tem enxaqueca, e decidimos fazer o uso do óleo de CBD. As crises diminuíram e a intensidade delas também.',
-      image: 'assets/fernanda.jpg'
-    }
-    // Add more feedbacks as needed
-  ];
+  // feedbacks = [
+  //   {
+  //     name: 'Laércio da Costa',
+  //     text: 'Tenho diabetes tipo 2 e o óleo de CBD, com auxílio de um médico, me ajudou muito, pois as dores eram terríveis.',
+  //     image: 'assets/laercio.jpg'
+  //   },
+  //   {
+  //     name: 'Fernanda Silva',
+  //     text: 'Minha mãe tem enxaqueca, e decidimos fazer o uso do óleo de CBD. As crises diminuíram e a intensidade delas também.',
+  //     image: 'assets/fernanda.jpg'
+  //   }
+  //   // Add more feedbacks as needed
+  // ];
 
-  responsiveOptions = [
-    {
-      breakpoint: '1024px',
-      numVisible: 2,
-      numScroll: 1
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 1,
-      numScroll: 1
-    }
-  ];
+  // responsiveOptions = [
+  //   {
+  //     breakpoint: '1024px',
+  //     numVisible: 2,
+  //     numScroll: 1
+  //   },
+  //   {
+  //     breakpoint: '768px',
+  //     numVisible: 1,
+  //     numScroll: 1
+  //   }
+  // ];
 }
