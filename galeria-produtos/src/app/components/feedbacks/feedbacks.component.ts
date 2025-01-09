b150e01201153e21b2fc1c4f49448eec44d1acdb
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2025-01-09 19:39:35 -0300

    fix

--- galeria-produtos/src/app/components/feedbacks/feedbacks.component.ts
+++ galeria-produtos/src/app/components/feedbacks/feedbacks.component.ts
@@ -1,47 +1,49 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'primeng/carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports : [CommonModule],
+  imports: [CarouselModule, CommonModule],
   templateUrl: './feedbacks.component.html',
 
 })
 export class FeedbacksComponent {
-  feedbacks = [
+  responsiveOptions = [
     {
-      name: 'Laércio da Costa',
-      message:
-        'Tenho diabetes tipo 2 e o óleo de CBD, com auxílio de um médico, me ajudou muito, pois as dores eram terríveis.',
-      image: 'assets/media/user1.jpg',
-      isActive: false,
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 1,
     },
     {
-      name: 'Fernanda Silva',
-      message:
-        'Minha mãe tem enxaqueca, e decidimos fazer o uso do óleo de CBD. As crises diminuíram e a intensidade delas também.',
-      image: 'assets/media/user2.jpg',
-      isActive: true,
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 1,
     },
     {
-      name: 'João Oliveira',
-      message:
-        'O óleo de CBD foi um divisor de águas na minha qualidade de vida. Sinto-me muito mais disposto e com menos dores.',
-      image: 'assets/media/user3.jpg',
-      isActive: false,
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
     },
   ];
 
-  activeIndex = 1;
 
-  get paginationDots() {
-    return Array(this.feedbacks.length);
-  }
-
-  changeActiveFeedback(index: number) {
-    this.activeIndex = index;
-    this.feedbacks.forEach((fb, i) => (fb.isActive = i === index));
-  }
+  feedbacks = [
+    {
+      name: 'Laércio da Costa',
+      message: 'Tenho diabetes tipo 2 e o óleo de CBD, com auxílio de um médico, me ajudou muito, pois as dores eram terríveis.',
+      image: '../../../assets/media/ampola.svg'
+    },
+    {
+      name: 'Fernanda Silva',
+      message: 'Minha mãe tem enxaqueca, e decidimos fazer o uso do óleo de CBD. As crises diminuíram e a intensidade delas também.',
+      image: '../../../assets/media/ampola.svg'
+    },
+    {
+      name: 'Carlos Santos',
+      message: 'Com o uso do óleo, notei uma melhora significativa no meu sono e redução da ansiedade.',
+     image: '../../../assets/media/ampola.svg'
+    }
+  ];
 }
