7f0b6dca2319fed2386e62dcad153aea4eacf3d9
Alex_Pasini <alexantoiopasini20@gmail.com>
2025-01-10 13:28:12 -0300

    .

--- src/app/shared/send_mesage.ts
+++ src/app/shared/send_mesage.ts
@@ -7,13 +7,13 @@ export class SendWhats{
 
     let infoMesage = `\n *Total:* R$ ${totalPrice}\n\n`
     // let finalMesage = initiaMesage +itemsMesage+infoMesage
-    let finalMesage = "Olá, vim pelo site e gostaria de saber mais informações sobre os produtos.\n\n"
+    let finalMesage = "Olá, vim pelo site da CBD Vital Med e gostaria de saber mais sobre o óleo de Cannabidiol.\n\n"
     return finalMesage
   }
   sendMensage() {
     // Codifica a mensagem para a URL
     const encodedMessage = encodeURIComponent(this.formatMasage());
-    const href = `https://wa.me/+5555999177627?text=${encodedMessage}`;
+    const href = `https://wa.me/+5547992122928?text=${encodedMessage}`;
     console.log(href)
 
     // Abre o WhatsApp em uma nova aba
